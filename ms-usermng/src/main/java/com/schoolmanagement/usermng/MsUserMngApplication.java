a3d2207287c8ae39d6e429a803c761dc5a18255f
familyphysician <sarathigoswami@gmail.com>
2025-09-24 14:38:48 +0530

    Update
    Configuration

---
+++ ms-usermng/src/main/java/com/schoolmanagement/usermng/MsUserMngApplication.java
@@ -0,0 +1,13 @@
+package com.schoolmanagement.usermng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserMngApplication.class, args);
+	}
+
+}
---
+++ ms-usermng/src/test/java/com/schoolmanagement/usermng/MsUserMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolmanagement.usermng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
